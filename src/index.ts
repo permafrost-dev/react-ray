43519f7aea1f7fd8a9e088149fcf0697b7fe66f0
Patrick Organ <patrick@permafrost.dev>
2023-03-11 02:27:33 -0500

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-console.log('hello world');
+import { ray } from 'node-ray/web';
+import { useRef, useEffect } from 'react';
 
-export function add(a: number, b: number) {
-    return a + b;
-}
+export const useRay = (...args) => {
+    return ray(...args);
+};
+
+export const useRaySequentialRef = (ref, deps = []) => {
+    useEffect(() => {
+        ray().html(ref.current.innerHTML);
+    }, [ ref, ...deps ]);
+};
+
+export const useRayTrackRef = (ref, deps = []) => {
+    const rayRef = useRef(null);
+
+    useEffect(() => {
+        const r = rayRef.current ?? ray();
+        rayRef.current = r.html(ref.current.innerHTML);
+    }, [ ref, ...deps ]);
+};
