324434f5825ebc5a9911c9c44e49efe075292573
patinthehat <patinthehat@users.noreply.github.com>
2023-05-18 23:17:28 +0000

    Fix styling

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 const { pathsToModuleNameMapper } = require('ts-jest');
 const tsConfigPaths = {
-    '@/*': [ 'src/*' ],
-    '@tests/*': [ 'tests/*' ],
+    '@/*': ['src/*'],
+    '@tests/*': ['tests/*'],
 };
 
 module.exports = {
@@ -9,23 +9,11 @@ module.exports = {
     testEnvironment: 'jsdom',
     transform: { '^.+\\.tsx?$': 'ts-jest' },
     testRegex: '(/__test__/.*|/tests/.*|(\\.|/)(test|spec))\\.[tj]sx?$',
-    testPathIgnorePatterns: [ '/node_modules/', '/dist/' ],
-    moduleFileExtensions: [
-        'ts',
-        'tsx',
-        'js',
-        'jsx',
-        'json'
-    ],
+    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
     moduleNameMapper: pathsToModuleNameMapper(tsConfigPaths, { prefix: `${__dirname}/` }),
 
     coverageDirectory: './coverage',
-    coverageReporters: [ 'html', 'text' ],
-    collectCoverageFrom: [
-        'src/**/*.{ts,js}',
-        '!**/node_modules/**',
-        '!**/vendor/**',
-        '!**/dist/**',
-        '!**/tests/**'
-    ],
+    coverageReporters: ['html', 'text'],
+    collectCoverageFrom: ['src/**/*.{ts,js}', '!**/node_modules/**', '!**/vendor/**', '!**/dist/**', '!**/tests/**'],
 };
